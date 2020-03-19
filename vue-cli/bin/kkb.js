d4e0817ff7515e6e603d607b54362df19373a2e9
LF90187 <piao_minqing@nhn-st.com>
2020-03-19 17:49:38 +0800

    update

--- vue-cli/bin/kkb.js
+++ vue-cli/bin/kkb.js
@@ -1,1 +1,15 @@
-console.log('..........................')
+#!/usr/bin/env node
+console.log('cli....................')
+
+const program = require('commander')
+
+program.version(require('../package').version)
+
+program
+  .command('init <name>')
+  .description('init project')
+  .action(name => {
+    require('../lib/init')()
+  })
+
+program.parse(process.argv)
